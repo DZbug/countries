cecb5c259e0ef6c102b24debcd15498fd8892e16
dzbugeek <dzbugeek@gmail.com>
2020-07-25 12:41:02 +0300

    Grid

--- src/components/countries/CountryItem.js
+++ src/components/countries/CountryItem.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const CountryItem = () => {
   return (
-    <div className="card country-item">
-      <h3>Germany</h3>
-      <p>
-        <span className="country-description">Population:</span> 81,770,900
-      </p>
-      <p>
-        <span className="country-description">Region:</span> Europe
-      </p>
-      <p>
-        <span className="country-description">Capital:</span> Berlin
-      </p>
+    <div className="country-item">
+      <div className="card">
+        <h3>Germany</h3>
+        <p>
+          <span className="country-description">Population:</span> 81,770,900
+        </p>
+        <p>
+          <span className="country-description">Region:</span> Europe
+        </p>
+        <p>
+          <span className="country-description">Capital:</span> Berlin
+        </p>
+      </div>
     </div>
   );
 };
